084d1bf72afed44f83d69193289d4e064e7df619
mingosweb <mingox123@gmail.com>
2015-03-17 00:03:24 -0500

    agregado modulo que incrusta las librerias js y estilos en el encabezado de Page.php

---
+++ vista/modulos/script.sesion.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" type="text/css" href="vista/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="vista/css/estiloPrincipal.css" />
+<script src="vista/js/jquery-1.11.2.min.js"></script>
+<script src="vista/js/jquery-validation-1.13.1/dist/jquery.validate.min.js"></script>
+<script src="vista/js/validar.js"></script>
